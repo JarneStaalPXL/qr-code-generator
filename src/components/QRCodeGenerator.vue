89440a04fe9e42939c291fb49e892e3dc5e1b4ec
DeveloperJarne <jarne.staal9@outlook.com>
2024-04-23 21:49:22 +0200

    base qrcode generation

---
+++ src/components/QRCodeGenerator.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="qr-code-generator">
+        <h1>QR Code Generator</h1>
+        <input v-model="url" placeholder="Enter URL here..." type="text">
+        <button @click="generateQRCode">Generate QR Code</button>
+        <div v-if="qrCodeDataUrl">
+            <img :src="qrCodeDataUrl" alt="Generated QR Code" />
+            <button @click="downloadImage">Download QR Code</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import QRCode from 'qrcode'
+
+export default {
+    name: 'QRCodeGenerator',
+    data() {
+        return {
+            url: '',
+            qrCodeDataUrl: null
+        }
+    },
+    methods: {
+        async generateQRCode() {
+            try {
+                this.qrCodeDataUrl = await QRCode.toDataURL(this.url);
+            } catch (error) {
+                console.error('Failed to generate QR Code:', error);
+            }
+        },
+        downloadImage() {
+            // Create a temporary link element
+            const link = document.createElement('a');
+            link.href = this.qrCodeDataUrl;
+            // Set the download attribute with a default file name
+            link.download = 'QRCode.png';
+            // Append the link to the body
+            document.body.appendChild(link);
+            // Trigger the download by simulating click
+            link.click();
+            // Remove the link after downloading
+            document.body.removeChild(link);
+        }
+    }
+}
+</script>
+
+<style>
+.qr-code-generator {
+    text-align: center;
+    margin-top: 50px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,13 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <QRCodeGenerator></QRCodeGenerator>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import QRCodeGenerator from '../components/QRCodeGenerator.vue'
 export default {
-  name: 'HomeView',
+  name: 'App',
   components: {
-    HelloWorld
+    QRCodeGenerator
   }
 }
 </script>
