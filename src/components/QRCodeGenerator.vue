291bf82089fb1d858f1cb5c53bf5173ecd8fba2f
DeveloperJarne <jarne.staal9@outlook.com>
2024-04-24 21:20:07 +0200

    history of QR codes

--- src/components/QRCodeGenerator.vue
+++ src/components/QRCodeGenerator.vue
@@ -4,19 +4,52 @@
         <div class="inputcontainer">
             <input
                 class="block rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                v-model="url" placeholder="Plak de link hier..." type="text" />
+                v-model="url" placeholder="Plak de link hier..." type="text" @keydown.enter="generateQRCode()" />
         </div>
 
-        <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+        <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded genDownBtn"
             @click="generateQRCode">
             Genereer QR Code
         </button>
         <div v-if="qrCodeDataUrl" class="qrcodecontainer">
             <img :src="qrCodeDataUrl" alt="Generated QR Code" class="qrcode" />
-            <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded"
+            <button class="bg-indigo-600 hover:bg-indigo-700 text-white font-bold py-2 px-4 rounded genDownBtn"
                 @click="downloadImage">
                 Download QR Code
             </button>
+        </div>
+
+        <div class="savedQrCodes" v-if="savedQRs.length > 0">
+            <table class="table-auto">
+                <thead>
+                    <tr>
+                        <th class="px-4 py-2 hideColumn">QR Code</th>
+                        <th class="px-4 py-2">URL</th>
+                        <th class="px-4 py-2">Actie</th>
+                        <th class="px-4 py-2 hideColumn">Verwijder</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr v-for="(item, index) in reversedSavedQRs" :key="index">
+                        <td class="border px-4 py-2 hideColumn">
+                            <img :src="item.dataUrl" alt="Saved QR Code" class="qrcode" />
+                        </td>
+                        <td class="border px-4 py-2" style="max-width: 200px;word-wrap: break-word;">
+                            {{ item.url }}
+                        </td>
+                        <td class="border px-4 py-2">
+                            <button @click="downloadImageFromElement(item)"
+                                class="bg-indigo-600 hover:bg-indigo-700 text-white rounded downloadBtnTable">Download
+                                QR Code</button>
+                        </td>
+                        <td class="border px-4 py-2 hideColumn">
+                            <button @click="removeItemFromSavedQRs(item);"
+                                class="bg-red-600 hover:bg-red-700 text-white rounded downloadBtnTable">Verwijder</button>
+                        </td>
+                    </tr>
+                </tbody>
+
+            </table>
         </div>
     </div>
 </template>
@@ -30,19 +63,35 @@ export default {
         return {
             url: "",
             qrCodeDataUrl: null,
+            savedQRs: [],
         };
     },
+    created() {
+        this.loadSavedQRs();
+    },
+    computed: {
+        reversedSavedQRs() {
+            // Return a reversed copy of the saved QRs array
+            return [...this.savedQRs].reverse();
+        }
+    },
     methods: {
+        removeItemFromSavedQRs(item) {
+            this.savedQRs = this.savedQRs.filter((qr) => qr.url !== item.url);
+            localStorage.setItem("qrCodes", JSON.stringify(this.savedQRs));
+        },
         async generateQRCode() {
+
             const options = {
-                errorCorrectionLevel: "H", // High error correction level
+                errorCorrectionLevel: "H",
                 type: "image/png",
-                quality: 1, // Highest quality
+                quality: 1,
                 margin: 1,
-                scale: 10, // Higher scale for better resolution
+                scale: 10,
             };
             try {
                 this.qrCodeDataUrl = await QRCode.toDataURL(this.url, options);
+                this.saveQR();
             } catch (error) {
                 console.error("Failed to generate QR Code:", error);
             }
@@ -55,6 +104,30 @@ export default {
             link.click();
             document.body.removeChild(link);
         },
+        downloadImageFromElement(el) {
+            const link = document.createElement("a");
+            link.href = el.dataUrl;
+            link.download = el.url + "-qrcode.png";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        },
+        saveQR() {
+
+            const qrData = { url: this.url, dataUrl: this.qrCodeDataUrl };
+
+            // Save the QR code data to local storage if it doesn't exist
+            if (!this.savedQRs.some((item) => item.url === this.url)) {
+                this.savedQRs.push(qrData);
+                localStorage.setItem("qrCodes", JSON.stringify(this.savedQRs));
+            }
+        },
+        loadSavedQRs() {
+            const savedData = localStorage.getItem('qrCodes');
+            if (savedData) {
+                this.savedQRs = JSON.parse(savedData);
+            }
+        }
     },
 };
 </script>
@@ -68,7 +141,7 @@ export default {
 }
 
 input,
-button {
+.genDownBtn {
     width: 300px;
 }
 
@@ -106,5 +179,29 @@ input {
     margin-bottom: 20px;
 }
 
-.qrcode {}
+.qrcode {
+    max-width: 100px;
+    max-height: 100px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
+
+.savedQrCodes {
+    display: flex;
+    justify-content: center;
+    margin-top: 100px;
+    margin-bottom: 300px;
+
+}
+
+.downloadBtnTable {
+    padding: 10px;
+}
+
+@media only screen and (max-width: 600px) {
+    .hideColumn {
+        display: none;
+    }
+}
 </style>
